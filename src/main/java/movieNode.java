68e35f833f26f6025ae5a45f72251cb524b1ad98
Cameron Aaron <cameronaaron1@gmail.com>
2018-04-30 18:06:15 -0400

    Init serializ

--- src/main/java/movieNode.java
+++ src/main/java/movieNode.java
@@ -1,53 +1,60 @@
-public class movieNode{
+public class movieNode {
 
-	///Creating the 4 instance variables/
-	private String title;
-	private int rDate;
-	private int popScore;
-	private int uniqID;
+    /// Creating the 4 instance variables/
+    private String title;
+    private int rDate;
+    private int popScore;
+    private int uniqID;
     private movieNode rightReleaseD;
     private movieNode leftReleaseD;
     private movieNode rightID;
     private movieNode leftID;
+    private float score;
 
-	///A constructor method which takes 4 parameters/
-	public movieNode(String title, int rDate, int popScore){
-		this.title = title;
-		this.rDate = rDate;
-		this.popScore = popScore;
+    /// A constructor method which takes 4 parameters/
+    public movieNode(String title, int rDate, int popScore) {
+        this.title = title;
+        this.rDate = rDate;
+        this.popScore = popScore;
         this.rightID = rightID;
         this.leftID = leftID;
         this.leftReleaseD = leftReleaseD;
         this.rightReleaseD = rightReleaseD;
+        this.score = score;
+    }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	}
-	public String getTitle(){
-		return title;
-	}
-	public void setTitle(String title){
-		this.title = title;
-	}
-	public int getRDate(){
-		return rDate;
-	}
-	public void setRDate(int rDate){
-		this.rDate = rDate;
-	}
-	public int getUniqID(){
-		int ID = rDate % 10;
-		return ID;
-	}
-	public void setUniqID(int uniqID){
-		this.uniqID = uniqID;
-	}
-	public int getPopScore(){
-		return popScore;
-	}
-	public void popScore(int popScore){
-		this.popScore = popScore;
-	}
+    public int getRDate() {
+        return rDate;
+    }
+
+    public void setRDate(int rDate) {
+        this.rDate = rDate;
+    }
+
+    public int getUniqID() {
+        int ID = rDate % 10;
+        return ID;
+    }
+
+    public void setUniqID(int uniqID) {
+        this.uniqID = uniqID;
+    }
+
+    public int getPopScore() {
+        return popScore;
+    }
+
+    public void popScore(int popScore) {
+        this.popScore = popScore;
+    }
 
     public movieNode getLeftReleaseD() {
         return leftReleaseD;
@@ -72,6 +79,7 @@ public class movieNode{
     public void setLeftID(movieNode leftID) {
         this.leftID = leftID;
     }
+
     public movieNode getrightID() {
         return rightID;
     }
@@ -79,4 +87,6 @@ public class movieNode{
     public void setRiightID(movieNode rightID) {
         this.rightID = rightID;
     }
-    }
+
+
+}
