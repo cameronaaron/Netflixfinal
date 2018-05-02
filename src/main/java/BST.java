942d02869679d3551d5743b6262a5a7796ded9f2
TheSauce14 <38663130+TheSauce14@users.noreply.github.com>
2018-05-02 11:18:59 -0400

    Update BST.java

--- src/main/java/BST.java
+++ src/main/java/BST.java
@@ -1,14 +1,15 @@
-//Cameron Aaron
 //Mar 23 2018
 public class BST {
     //Accumulator
     private movieNode root;
+    private movieNode root2;
 
 
     //Declaire public class
     public BST() {
         //Accumulator starts at 0
         root = null;
+        root2 = null;
     }
 
     // Returns true if the list is empty
@@ -168,6 +169,172 @@ public class BST {
                 System.out.println("Right: null ");
             printTree2(tree.getLeftReleaseD());
             printTree2(tree.getRightReleaseD());
+        }
+    }
+
+
+
+
+
+
+
+    // Returns true if the list is empty
+    public boolean isEmptyTreeID() {
+        return (root2 == null);
+    }
+
+    // Pop Funtion
+    public void deleteID(movieNode x) {
+        if (x == root2) {
+            root = deleteRootID(x);
+
+        } else {
+            delete2ID(root2, x);
+        }
+
+
+    }
+
+    public movieNode deleteRootID(movieNode x) {
+        if (x.getRightID() == null && x.getLeftID() == null) {
+            return null;
+        } else if (x.getLeftID() == null) {
+            movieNode temp = x.getRightID();
+            x.setRightID(null);
+            return temp;
+        } else if (x.getLeftID() == null) {
+            movieNode temp = x.getRightID();
+            x.setLeftID(null);
+            return temp;
+        } else {
+            movieNode s = successorID(x);
+            delete(s);
+            s.setRightID(x.getRightID());
+            s.setLeftID(x.getLeftID());
+            x.setLeftID(null);
+            x.setRightID(null);
+            return s;
+
+
+        }
+    }
+
+
+    public movieNode successorID(movieNode x) {
+        movieNode temp = x.getRightID();
+        while (temp.getLeftID() != null) {
+            temp = temp.getLeftID();
+        }
+        return temp;
+    }
+
+    public void delete2ID(movieNode tree, movieNode x) {
+        if (tree.getRightID() == x) {
+
+            tree.setRightID(deleteRoot(x));
+        } else if (tree.getLeftID() == x) {
+            tree.setLeftID(deleteRoot(x));
+        } else if (x.getUniqID() < tree.getUniqID()) {
+            delete2(tree.getLeftID(), x);
+        } else {
+            delete2(tree.getRightID(), x);
+        }
+
+
+    }
+
+    // Return the top node in stac
+    // pushes node to top of stack
+    public void insertID(movieNode newNode) {
+        if (null == root2) {
+            root2 = newNode;
+
+        } else {
+            insert2ID(root2, newNode);
+        }
+
+
+    }
+
+    public void insert2ID(movieNode t, movieNode p) {
+        if (p.getUniqID() < t.getUniqID()) {
+            if (t.getLeftID() == null) {
+                t.setLeftID(p);
+            } else {
+                insert2(t.getLeftID(), p);
+
+            }
+
+        } else {
+            if (t.getRightID() == null) {
+                t.setRightID(p);
+            } else {
+                insert2(t.getRightID(), p);
+
+            }
+
+        }
+    }
+
+    public movieNode searchID(int key) {
+        if (root.getUniqID() == key) {
+            return root2;
+        } else {
+            return searchRID(root2, key);
+        }
+
+
+    }
+
+    public movieNode searchRID(movieNode t, int key) {
+        if (t == null) {
+            return null;
+        } else if (key == t.getUniqID()) {
+            return t;
+        } else if (key < t.getUniqID()) {
+            return searchR(t.getLeftID(), key);
+
+        } else {
+            return searchR(t.getRightID(), key);
+
+        }
+    }
+
+    public void traverseID() {
+        if (root2 != null) {
+            traverse2ID(root2);
+
+        }
+    }
+
+    public void traverse2ID(movieNode t) {
+        if (t != null) {
+            traverse2ID(t.getLeftID());
+            System.out.println(t.getUniqID());
+            traverse2ID(t.getRightID());
+        }
+    }
+
+
+    //Prints the stack
+    public void printTreeID() {
+        printTree2ID(root2);
+        System.out.println();
+    }
+
+    private void printTree2ID(movieNode tree) {
+        if (tree != null) {
+            System.out.print(tree.getUniqID() + " ");
+            if (tree.getLeftID() != null)
+                System.out.print("Left: " + tree.getLeftID().getUniqID() + " ");
+            else
+                System.out.print("Left: null ");
+            if (tree.getRightID() != null)
+                System.out.println("Right: " + tree.getRightID().getUniqID() + " ");
+            else
+                System.out.println("Right: null ");
+            printTree2(tree.getLeftID());
+            printTree2(tree.getRightID());
         }
     }
 
