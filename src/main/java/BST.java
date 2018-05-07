8b55dc0f867562851343b8b9c605ca6b5254ad42
TheSauce14 <38663130+TheSauce14@users.noreply.github.com>
2018-05-07 11:43:49 -0400

    Update BST.java

--- src/main/java/BST.java
+++ src/main/java/BST.java
@@ -1,341 +1,277 @@
-//Mar 23 2018
-public class BST {
-    //Accumulator
+public class BST{
+
     private movieNode root;
     private movieNode root2;
 
-
-    //Declaire public class
-    public BST() {
-        //Accumulator starts at 0
+    public BST(){
         root = null;
         root2 = null;
+
     }
 
-    // Returns true if the list is empty
-    public boolean isEmptyTree() {
-        return (root == null);
-    }
-
-    // Pop Funtion
-    public void delete(movieNode x) {
-        if (x == root) {
-            root = deleteRoot(x);
-
-        } else {
-            delete2(root, x);
+    public Boolean isEmptyTree(){
+        if(root == null){
+            return true;
         }
-
-
-    }
-
-    public movieNode deleteRoot(movieNode x) {
-        if (x.getRightReleaseD() == null && x.getLeftReleaseD() == null) {
-            return null;
-        } else if (x.getLeftReleaseD() == null) {
-            movieNode temp = x.getRightReleaseD();
-            x.setRightReleaseD(null);
-            return temp;
-        } else if (x.getLeftReleaseD() == null) {
-            movieNode temp = x.getRightReleaseD();
-            x.setLeftReleaseD(null);
-            return temp;
-        } else {
-            movieNode s = successor(x);
-            delete(s);
-            s.setRightReleaseD(x.getRightReleaseD());
-            s.setLeftReleaseD(x.getLeftReleaseD());
-            x.setLeftReleaseD(null);
-            x.setRightReleaseD(null);
-            return s;
-
-
+        else{
+            return false;
         }
     }
 
-
-    public movieNode successor(movieNode x) {
-        movieNode temp = x.getRightReleaseD();
-        while (temp.getLeftReleaseD() != null) {
-            temp = temp.getLeftReleaseD();
-        }
-        return temp;
-    }
-
-    public void delete2(movieNode tree, movieNode x) {
-        if (tree.getRightReleaseD() == x) {
-
-            tree.setRightReleaseD(deleteRoot(x));
-        } else if (tree.getLeftReleaseD() == x) {
-            tree.setLeftReleaseD(deleteRoot(x));
-        } else if (x.getRDate() < tree.getRDate()) {
-            delete2(tree.getLeftReleaseD(), x);
-        } else {
-            delete2(tree.getRightReleaseD(), x);
-        }
-
-
-    }
-
-    // Return the top node in stac
-    // pushes node to top of stack
-    public void insert(movieNode newNode) {
-        if (null == root) {
-            root = newNode;
-
-        } else {
-            insert2(root, newNode);
-        }
-
-
-    }
-
-    public void insert2(movieNode t, movieNode p) {
-        if (p.getRDate() < t.getRDate()) {
-            if (t.getLeftReleaseD() == null) {
-                t.setLeftReleaseD(p);
-            } else {
-                insert2(t.getLeftReleaseD(), p);
-
-            }
-
-        } else {
-            if (t.getRightReleaseD() == null) {
-                t.setRightReleaseD(p);
-            } else {
-                insert2(t.getRightReleaseD(), p);
-
-            }
-
-        }
-    }
-
-    public movieNode search(int key) {
-        if (root.getRDate() == key) {
-            return root;
-        } else {
-            return searchR(root, key);
-        }
-
-
-    }
-
-    public movieNode searchR(movieNode t, int key) {
-        if (t == null) {
-            return null;
-        } else if (key == t.getRDate()) {
-            return t;
-        } else if (key < t.getRDate()) {
-            return searchR(t.getLeftReleaseD(), key);
-
-        } else {
-            return searchR(t.getRightReleaseD(), key);
-
-        }
-    }
-
-    public void traverse() {
-        if (root != null) {
-            traverse2(root);
-
-        }
-    }
-
-    public void traverse2(movieNode t) {
-        if (t != null) {
-            traverse2(t.getLeftReleaseD());
-            System.out.println(t.getRDate());
-            traverse2(t.getRightReleaseD());
-        }
-    }
-
-
-    //Prints the stack
     public void printTree() {
-        printTree2(root);
-        System.out.println();
+    printTree2(root);
+    System.out.println();
     }
 
     private void printTree2(movieNode tree) {
-        if (tree != null) {
-            System.out.print(tree.getRDate() + " ");
+    if (tree != null) {
+        System.out.print(tree.getRDate() + " ");
             if (tree.getLeftReleaseD() != null)
-                System.out.print("Left: " + tree.getLeftReleaseD().getRDate() + " ");
+            System.out.print("Left: " + tree.getLeftReleaseD().getRDate() + " ");
             else
                 System.out.print("Left: null ");
             if (tree.getRightReleaseD() != null)
-                System.out.println("Right: " + tree.getRightReleaseD().getRDate() + " ");
+            System.out.println("Right: " + tree.getRightReleaseD().getRDate() + " ");
             else
                 System.out.println("Right: null ");
-            printTree2(tree.getLeftReleaseD());
-            printTree2(tree.getRightReleaseD());
+        printTree2(tree.getLeftReleaseD());
+        printTree2(tree.getRightReleaseD());
+    }
+    }
+
+    public void insert(movieNode x){
+        if (root == null){
+            root = x;
+        }
+        else{
+            insert2(root, x);
         }
     }
 
-
-
-
-
-
-
-    // Returns true if the list is empty
-    public boolean isEmptyTreeID() {
-        return (root2 == null);
-    }
-
-    // Pop Funtion
-    public void deleteID(movieNode x) {
-        if (x == root2) {
-            root2 = deleteRootID(x);
-
-        } else {
-            delete2ID(root2, x);
-        }
-
-
-    }
-
-    public movieNode deleteRootID(movieNode x) {
-        if (x.getRightID() == null && x.getLeftID() == null) {
-            return null;
-        } else if (x.getLeftID() == null) {
-            movieNode temp = x.getRightID();
-            x.setRightID(null);
-            return temp;
-        } else if (x.getLeftID() == null) {
-            movieNode temp = x.getRightID();
-            x.setLeftID(null);
-            return temp;
-        } else {
-            movieNode s = successorID(x);
-            deleteID(s);
-            s.setRightID(x.getRightID());
-            s.setLeftID(x.getLeftID());
-            x.setLeftID(null);
-            x.setRightID(null);
-            return s;
-
-
-        }
-    }
-
-
-    public movieNode successorID(movieNode x) {
-        movieNode temp = x.getRightID();
-        while (temp.getLeftID() != null) {
-            temp = temp.getLeftID();
-        }
-        return temp;
-    }
-
-    public void delete2ID(movieNode tree, movieNode x) {
-        if (tree.getRightID() == x) {
-
-            tree.setRightID(deleteRoot(x));
-        } else if (tree.getLeftID() == x) {
-            tree.setLeftID(deleteRoot(x));
-        } else if (x.getUniqID() < tree.getUniqID()) {
-            delete2ID(tree.getLeftID(), x);
-        } else {
-            delete2ID(tree.getRightID(), x);
-        }
-
-
-    }
-
-    // Return the top node in stac
-    // pushes node to top of stack
-    public void insertID(movieNode newNode) {
-        if (null == root2) {
-            root2 = newNode;
-
-        } else {
-            insert2ID(root2, newNode);
-        }
-
-
-    }
-
-    public void insert2ID(movieNode t, movieNode p) {
-        if (p.getUniqID() < t.getUniqID()) {
-            if (t.getLeftID() == null) {
-                t.setLeftID(p);
-            } else {
-                insert2ID(t.getLeftID(), p);
-
+    public void insert2(movieNode r, movieNode x){
+        if(x.getRDate() < r.getRDate()){
+            if(r.getLeftReleaseD() == null){
+                r.setLeftReleaseD(x);
+            
             }
-
-        } else {
-            if (t.getRightID() == null) {
-                t.setRightID(p);
-            } else {
-                insert2ID(t.getRightID(), p);
-
+            else{
+                insert2(r.getLeftReleaseD(), x);
+            }}
+        else{
+            if(r.getRightReleaseD() == null){
+                r.setRightReleaseD(x);
+        }
+            else{
+                insert2(r.getRightReleaseD(), x);
             }
-
         }
     }
+    
+    
+    public void traverse(){
+    traverser(root);
+    }
 
-    public movieNode searchID(int key) {
-        if (root.getUniqID() == key) {
-            return root2;
-        } else {
-            return searchRID(root2, key);
-        }
-
+    public void traverser(movieNode x){
+    if(x != null){
+        traverser(x.getLeftReleaseD());
+        System.out.println(x.getRDate());
+        traverser(x.getRightReleaseD());
+    }
+    }
+    
+    public movieNode search(int key){
+    return searchr(root, key);
 
     }
 
-    public movieNode searchRID(movieNode t, int key) {
-        if (t == null) {
+    public movieNode searchr(movieNode x, int key){
+    if(x == null){
+        return x;
+        }
+    else if(key == x.getRDate()){
+        return x;
+    }
+    else if(key < x.getRDate()){
+        return searchr(x.getLeftReleaseD(), key);
+    }
+    else{
+        return searchr(x.getRightReleaseD(), key);
+    }
+    }
+
+public void delete(movieNode p){
+        // Removes the node p from the tree
+        if (root!=null){// if the tree is not empty calls delete2
+            delete2(root,p);
+        }
+    }
+    public movieNode delete2(movieNode parent, movieNode p){
+        //called on delete to recursively delete the node p
+        if (parent==null){// if the Node being passes is null returns null
             return null;
-        } else if (key == t.getUniqID()) {
-            return t;
-        } else if (key < t.getUniqID()) {
-            return searchR(t.getLeftID(), key);
+        }
+        if (parent.getRDate()==p.getRDate()){// if the parent is the desired node
+            if (parent.getRightReleaseD()==null && parent.getLeftReleaseD()==null){// Leaf
+                return null;
+            }
+            else if(parent.getRightReleaseD()!=null && parent.getLeftReleaseD()==null){// 1 child
+                return parent.getRightReleaseD();
+            }
+            else if(parent.getLeftReleaseD()!=null && parent.getRightReleaseD()==null){// 1 child
+                return parent.getLeftReleaseD();
+            }
+            else{// 2 childrem, uses the Node method findSuccessor to find the successor
+                movieNode suc= parent.getRightReleaseD().findSuccessorRDate();
+                parent.popScore(suc.getPopScore());
+                parent.setTitle(suc.getTitle());
+                parent.setRightReleaseD(delete2(parent.getRightReleaseD(), suc));
+                return parent;
+                }
+        }
+        else if (parent.getRDate()> p.getRDate()){// Calls delete2 on the left
+            parent.setLeftReleaseD(delete2(parent.getLeftReleaseD(),p));
+            return parent;
+        }
+        else if (parent.getRDate()< p.getRDate()){// calls delete2 on the Right
+            parent.setRightReleaseD(delete2(parent.getRightReleaseD(),p));
+            return parent;
+        }
+        
+        return null; 
+    }
 
-        } else {
-            return searchR(t.getRightID(), key);
-
+   public Boolean isEmptyTreeID(){
+        if(root2 == null){
+            return true;
+        }
+        else{
+            return false;
         }
     }
 
-    public void traverseID() {
-        if (root2 != null) {
-            traverse2ID(root2);
-
-        }
-    }
-
-    public void traverse2ID(movieNode t) {
-        if (t != null) {
-            traverse2ID(t.getLeftID());
-            System.out.println(t.getUniqID());
-            traverse2ID(t.getRightID());
-        }
-    }
-
-
-    //Prints the stack
     public void printTreeID() {
-        printTree2ID(root2);
-        System.out.println();
+    printTree2ID(root2);
+    System.out.println();
     }
 
     private void printTree2ID(movieNode tree) {
-        if (tree != null) {
-            System.out.print(tree.getUniqID() + " ");
+    if (tree != null) {
+        System.out.print(tree.getUniqID() + " ");
             if (tree.getLeftID() != null)
-                System.out.print("Left: " + tree.getLeftID().getUniqID() + " ");
+            System.out.print("Left: " + tree.getLeftID().getUniqID() + " ");
             else
                 System.out.print("Left: null ");
             if (tree.getRightID() != null)
-                System.out.println("Right: " + tree.getRightID().getUniqID() + " ");
+            System.out.println("Right: " + tree.getRightID().getUniqID() + " ");
             else
                 System.out.println("Right: null ");
-            printTree2(tree.getLeftID());
-            printTree2(tree.getRightID());
+        printTree2ID(tree.getLeftID());
+        printTree2ID(tree.getRightID());
+    }
+    }
+
+    public void insertID(movieNode x){
+        if (root2 == null){
+            root2 = x;
         }
+        else{
+            insert2ID(root2, x);
+        }
+    }
+
+    public void insert2ID(movieNode r, movieNode x){
+        if(x.getUniqID() < r.getUniqID()){
+            if(r.getLeftID() == null){
+                r.setLeftID(x);
+            
+            }
+            else{
+                insert2ID(r.getLeftID(), x);
+            }}
+        else{
+            if(r.getRightID() == null){
+                r.setRightID(x);
+        }
+            else{
+                insert2ID(r.getRightID(), x);
+            }
+        }
+    }
+    
+    
+    public void traverseID(){
+    traverserID(root);
+    }
+
+    public void traverserID(movieNode x){
+    if(x != null){
+        traverser(x.getLeftID());
+        System.out.println(x.getUniqID());
+        traverser(x.getRightID());
+    }
+    }
+    
+    public movieNode searchID(int key){
+    return searchrID(root2, key);
+
+    }
+
+    public movieNode searchrID(movieNode x, int key){
+    if(x == null){
+        return x;
+        }
+    else if(key == x.getUniqID()){
+        return x;
+    }
+    else if(key < x.getUniqID()){
+        return searchrID(x.getLeftID(), key);
+    }
+    else{
+        return searchrID(x.getRightID(), key);
+    }
+    }
+
+public void deleteID(movieNode p){
+        // Removes the node p from the tree
+        if (root2!=null){// if the tree is not empty calls delete2
+            delete2ID(root2,p);
+        }
+    }
+    public movieNode delete2ID(movieNode parent, movieNode p){
+        //called on delete to recursively delete the node p
+        if (parent==null){// if the Node being passes is null returns null
+            return null;
+        }
+        if (parent.getUniqID()==p.getUniqID()){// if the parent is the desired node
+            if (parent.getRightID()==null && parent.getLeftID()==null){// Leaf
+                return null;
+            }
+            else if(parent.getRightID()!=null && parent.getLeftID()==null){// 1 child
+                return parent.getRightID();
+            }
+            else if(parent.getLeftID()!=null && parent.getRightID()==null){// 1 child
+                return parent.getLeftID();
+            }
+            else{// 2 childrem, uses the Node method findSuccessor to find the successor
+                movieNode suc= parent.getRightID().findSuccessorID();
+                parent.popScore(suc.getPopScore());
+                parent.setTitle(suc.getTitle());
+                parent.setRDate(suc.getRDate());
+                parent.setRightID(delete2(parent.getRightID(), suc));
+                return parent;
+                }
+        }
+        else if (parent.getUniqID()> p.getUniqID()){// Calls delete2 on the left
+            parent.setLeftID(delete2(parent.getLeftID(),p));
+            return parent;
+        }
+        else if (parent.getUniqID()< p.getUniqID()){// calls delete2 on the Right
+            parent.setRightID(delete2(parent.getRightID(),p));
+            return parent;
+        }
+        
+        return null; 
     }
 
 }
