a06b34d25e156b03bfe935c131bf0a49c2136dcd
neliaaa <38664110+neliaaa@users.noreply.github.com>
2018-05-09 00:08:54 -0400

    Update netflix.java

--- src/main/java/netflix.java
+++ src/main/java/netflix.java
@@ -4,9 +4,13 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.io.ObjectInputStream;
+import java.io.*;
 
 public class netflix implements Serializable{
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+    Database a = new Database();
+    wishList w = new wishList();
         Scanner input1 = new Scanner(System.in);
         int c1;
         System.out.println("Welcome to the Netflix Database \n");
@@ -37,6 +41,7 @@ public class netflix implements Serializable{
                 System.out.println("Would you like to use a Loaded or New Database?");
                 System.out.println("(1) LOADED");
                 System.out.println("(2) NEW");
+                c3 = input3.nextInt();
                 if (c3 == 1) {
                     try {
                         FileInputStream fileIn = new FileInputStream("output.txt");
@@ -63,53 +68,88 @@ public class netflix implements Serializable{
                     System.out.println("(1) Access Movie(by ReleaseDate)");
                     System.out.println("(2) Access Movie(by ID)");
                     System.out.println("(3) Access Wish List");
-                    System.out.println("(4) Search Movie ID");
+                    System.out.println("(4) Order Movies by ReleaseDate");
                     System.out.println("(5) Return to Login");
                     System.out.println("(6) Quit"); //save info
                     System.out.println("Choice?");
                     c2 = input2.nextInt();
+                
+                    if (c2 == 1) {
+                        Scanner rdInput = new Scanner(System.in);
+                        System.out.println("Input Realease Date: ");
+                        int rdIn = rdInput.nextInt();
+                        System.out.println(a.accessMovie2(rdIn));
+                    }
+                    else if (c2 == 2) {
+                        Scanner rdInput = new Scanner(System.in);
+                        System.out.println("Input Realease Date: ");
+                        int rdIn = rdInput.nextInt();
+                        a.accessMovie(rdIn);
+                    }
+                    else if (c2 == 3) {
+                        Scanner input4 = new Scanner(System.in);
+                        int c4;
+                        System.out.println("WISHLIST-Choose an action below: \n");
+                        System.out.println("(1) Access Next Movie");
+                        c4 = input4.nextInt();
+                            if (c4 == 1){
+                                Scanner input5 = new Scanner(System.in);
+                                int c5;
+                                System.out.println("(1) Delete a movie");
+                                System.out.println("(2) Add a movie");
+                                c5 = input5.nextInt();
+                                if (c5 == 1) {
+                                    w.deleteMovie();
+                                }
+                                else if (c5 == 2){
+                                    System.out.println("mov1: Black Panther \nmov2: Avengers: Infinity War\nmov3: Pacific Rim-Uprising\nmov4: A Quiet Place\nmov5: Dunkirk\nmov6: Coco\nmov7: Moana\nmov8: Goal!\nmov9: Ferris Bueller's DAy Off\nmov10: Four Brothers\nmov11: Titanic\nmov12: The Wizard of Oz\nmov13: The Conjuring \nmov14: Insidious \nmov15: The Notebook\nmov16: 101 Dalmations\nmov17: Madea goes to Jail\nmov18: Get Out \nmov19: Cult of Chucky\nmov20: The Spongebob Squarepants Movie\nmov21: Home Alone 2\nmov22: The Shape of Water\nmov23: Fifty Shades Freed \nmov24: Camp Rock\nmov25: House of Wax\n");
+                                    Scanner input6 = new Scanner(System.in);
+                                    int c6;
+                                    System.out.println("How many movies would you like to add to WishList?");
+                                    c6 = input6.nextInt();
+                                    System.out.println("Which movie would you like to add?");
+                                    for(int i =0; i<= c6; i++){
+                                        Scanner input7 = new Scanner(System.in);
+                                        movieNode c7 = input7.nextline();
+                                        System.out.println(w.addMovie(c7));
+                                    }
+                                }
+                                else{
+                                    System.out.print("error");
+                                }
+                            }
+                            else{
+                                System.out.print("error");
+                            }
+                    }
+                    else if (c2 == 4) {
+                        a.printMovies();
+                    }
+                    else if (c2 == 5) {
+                            netflix.main(args);
+                    }
+                    else if (c2 == 6) {
+                        try {
+                            FileOutputStream fileOut = new FileOutputStream("output.txt");
+                            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+                            out.writeObject(a.netflixBST);
+                            out.writeObject(a.netflixHash);
+                            out.writeObject(a.netflixHeap);
+                            out.writeObject(a);
+                            out.close();
+                            fileOut.close();
+                            System.out.println("Serialized object successfully in output.txt");
+                            }
+                        catch(IOException i) {
+                                i.printStackTrace();
+                            }
+                            System.out.ln("You have successfully quit!");
+                    }
+                    else {
+                        System.out.println("Invalid Input");
+                    }
                 }
-                    // if (c2 == 1) {
-                    //     Scanner rdInput = new Scanner(System.in);
-                    //     System.out.println("Input Realease Date: ");
-                    //     string rdIn = rdInput.nextline();
-                    //     a.search(rdIn);
-                    // }
-                    // else if (c2 == 2) {
-
-                    // }
-                    // else if (c2 == 3) {
-                    //     wishListQueue a = new wishListQueue();
-                    // }
-                    // else if (c2 == 4) {
-
-                    // }
-                    // else if (c2 == 5) {
-                    //         netflix.main(args);
-                    // }
-                //     else if (c2 == 6) {
-                //         try {
-                //             FileOutputStream fileOut = new FileOutputStream("output.txt");
-                //             ObjectOutputStream out = new ObjectOutputStream(fileOut);
-                //             out.writeObject(a.netflixBST);
-                //             out.writeObject(a.netflixHash);
-                //             out.writeObject(a.netflixHeap);
-                //             out.writeObject(a);
-                //             out.close();
-                //             fileOut.close();
-                //             System.out.println("Serialized object successfully in output.txt");
-                //             }
-                //         catch(IOException i) {
-                //                 i.printStackTrace();
-                //             }
-                //     }
-                //     else {
-                //         System.out.println("Invalid Input");
-                //     }
-                // }
-            }
-
-                else if (c3 == 2){
+                else if(c3 == 2){
                     Scanner input2 = new Scanner(System.in);
                     int c2;
                     System.out.println("Choose an action below: \n");
@@ -162,8 +202,11 @@ public class netflix implements Serializable{
                 // }
                 // else {
                 //     System.out.println("Error");
-                 }
-
+                }
+                else{
+                    System.out.println("Error");
+                }
+            }
             else if (userU.equals(userNameU)) {
                 System.out.println("Invalid password");
                 netflix.main(args);
@@ -176,8 +219,8 @@ public class netflix implements Serializable{
                 System.out.println("Invalid username and password");
                 netflix.main(args);
             }
-        }
         
+        }
         else if (c1 == 2) {
             System.out.println("Hi Admin! \n");
 
