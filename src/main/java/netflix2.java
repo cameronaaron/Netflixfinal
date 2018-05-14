6bf723c56efab9060dbb9f0e41453671e8197180
Cameron Aaron <cameronaaron1@gmail.com>
2018-05-13 20:17:21 -0400

    TEST

--- src/main/java/netflix2.java
+++ src/main/java/netflix2.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.Scanner;
 
 public class netflix2 {
+    public movieNode zNode;
     public static void main(String[] args){
 
         netflixBST bst = new netflixBST();
@@ -46,22 +47,20 @@ public class netflix2 {
 
             if (c2 == 1) {
                 System.out.println("Hello New User!");
-                System.out.println("Please Input your Name:");
 
+                System.out.println("Please Input your Name:");
                 Scanner input3 = new Scanner(System.in);
                 String c3;
                 c3 = input3.nextLine();
 
 
                 System.out.println("Please Input your Credit Card Number:");
-
                 Scanner input4 = new Scanner(System.in);
                 int c4;
                 c4 = input4.nextInt();
 
 
                 System.out.println("Please Input your Email Address: ");
-
                 Scanner input5 = new Scanner(System.in);
                 String c5;
                 c5 = input5.nextLine();
@@ -74,6 +73,8 @@ public class netflix2 {
                 System.out.println(yNode.getCCN());
                 System.out.println(yNode.getEmail());
 
+
+                //s for customerNode
                 try {
                     FileOutputStream fileOut = new FileOutputStream("User.txt");
                     ObjectOutputStream out = new ObjectOutputStream(fileOut);
@@ -85,11 +86,11 @@ public class netflix2 {
                     i.printStackTrace();
                 }
 
+
                 System.out.println("Choose an action below: \n");
                 System.out.println("(1) Add movie");
                 System.out.println("(2) Access Movie(by ID)");
                 System.out.println("(3) Access Wish List");
-
                 System.out.println("(4) Print to Screen All Movies in Order of Release Date");
                 System.out.println("(5) Go back");
                 System.out.println("(6) Quit");
@@ -99,7 +100,6 @@ public class netflix2 {
                 Scanner inM1 = new Scanner(System.in);
                 int m1;
                 m1 = inM1.nextInt();
-
 
                 if (m1 == 1) {
                     //if serialization doesnt work then add the heap serialization
@@ -128,11 +128,11 @@ public class netflix2 {
                     System.out.println(zNode.getRDate());
                     System.out.println(zNode.getPopScore());
 
+                    //s for movieNode
                     try {
-                        FileOutputStream fileOut = new FileOutputStream("Movies.txt");
+                        FileOutputStream fileOut = new FileOutputStream("moviesBST.txt");
                         ObjectOutputStream out = new ObjectOutputStream(fileOut);
                         out.writeObject(bst);
-                        out.writeObject(heap);
                         out.close();
                         fileOut.close();
                         System.out.println("Your file has been serialized!");
@@ -140,24 +140,35 @@ public class netflix2 {
                         i.printStackTrace();
                     }
 
-
                     try {
-                        FileInputStream fileIn = new FileInputStream("Heap.txt");
-                        ObjectInputStream in = new ObjectInputStream(fileIn);
-                        heap = (netflixHeap) in.readObject();
-                        bst = (netflixBST) in.readObject();
-                        bst.search(zNode.getRDate());
-                        in.close();
-                        fileIn.close();
-                        // return heap;
+                        FileOutputStream fileOut = new FileOutputStream("moviesHeap.txt");
+                        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+                        out.writeObject(heap);
+                        out.close();
+                        fileOut.close();
+                        System.out.println("Your file has been serialized!");
                     } catch (IOException i) {
                         i.printStackTrace();
-                        //  return null;
-                    } catch (ClassNotFoundException c) {
-                        System.out.println("Error: Class not found");
-                        c.printStackTrace();
-                        // return null;
                     }
+                    // try{
+                    //     FileInputStream fileOut = new FileInputStream("Heap.txt");
+                    //     ObjectInputStream out = new ObjectInputStream(fileOut);
+                    //     heap = (netflixHeap) in.readObject();
+                    //     bst = (netflixBST) in.readObject();
+                    //     bst.search(zNode.getRDate());
+                    //     in.close();
+                    //     fileIn.close();
+                    //    // return heap;
+                    // }
+                    // catch(IOException i){
+                    //     i.printStackTrace();
+                    //   //  return null;
+                    // }
+                    // catch(ClassNotFoundException c) {
+                    //     System.out.println("Error: Class not found");
+                    //     c.printStackTrace();
+                    //    // return null;
+                    // }
                 } else if (m1 == 2) {
                     System.out.println("Enter ID: ");
                     Scanner uInput = new Scanner(System.in);
@@ -166,13 +177,14 @@ public class netflix2 {
                     i = uInput.nextInt();
                     System.out.println("The movie with ID " + i + " is: ");
 
-
+                    //d for movieNode for search by ID
                     try {
-                        FileInputStream fileIn = new FileInputStream("Heap.txt");
+                        FileInputStream fileIn = new FileInputStream("moviesBST.txt");
                         ObjectInputStream in = new ObjectInputStream(fileIn);
-                        heap = (netflixHeap) in.readObject();
+                        //heap = (netflixHeap) in.readObject();
                         bst = (netflixBST) in.readObject();
                         System.out.println(bst.searchID(i).getTitle());
+                        //System.out.println(heap.findMin().getTitle);
 
                         in.close();
                         fileIn.close();
@@ -244,6 +256,28 @@ public class netflix2 {
                     i = uInput.nextInt();
                     System.out.println("The movie with release date " + i + " is: ");
                     System.out.println(bst.search(i).getTitle());
+
+                    try {
+                        FileInputStream fileIn = new FileInputStream("moviesBST.txt");
+                        ObjectInputStream in = new ObjectInputStream(fileIn);
+                        //heap = (netflixHeap) in.readObject();
+                        bst = (netflixBST) in.readObject();
+                        //System.out.println(bst.searchID(i).getTitle());
+                        System.out.println(bst.search(i).getTitle());
+
+                        in.close();
+                        fileIn.close();
+                        // return heap;
+                    } catch (IOException w) {
+                        w.printStackTrace();
+                        //  return null;
+                    } catch (ClassNotFoundException c) {
+                        System.out.println("Error: Class not found");
+                        c.printStackTrace();
+                        // return null;
+                    }
+
+
                 } else {
                     System.out.println("Invaild Input. Please Try Again!");
 
@@ -259,6 +293,7 @@ public class netflix2 {
                 int ccn;
                 ccn = CCNInput.nextInt();
                 //hash = null;
+                //d for customerNode
                 try {
                     FileInputStream fileIn = new FileInputStream("User.txt");
                     ObjectInputStream in = new ObjectInputStream(fileIn);
@@ -277,6 +312,184 @@ public class netflix2 {
                     c.printStackTrace();
                     // return null;
                 }
+
+                System.out.println("Choose an action below: \n");
+                System.out.println("(1) Add movie");
+                System.out.println("(2) Access Movie(by ID)");
+                System.out.println("(3) Access Wish List");
+                System.out.println("(4) Print to Screen All Movies in Order of Release Date");
+                System.out.println("(5) Go back");
+                System.out.println("(6) Quit");
+                System.out.println("(7) Access Movie (by Release Date)");
+
+
+                Scanner inM1 = new Scanner(System.in);
+                int m1;
+                m1 = inM1.nextInt();
+
+                if (m1 == 1) {
+                    //if serialization doesnt work then add the heap serialization
+                    System.out.println("Input Movie Title: ");
+                    Scanner input20 = new Scanner(System.in);
+                    String c20;
+                    c20 = input20.nextLine();
+
+                    System.out.println("Input Release Date: ");
+                    Scanner input21 = new Scanner(System.in);
+                    int c21;
+                    c21 = input21.nextInt();
+
+                    System.out.println("Input Popularity Score: ");
+                    Scanner input22 = new Scanner(System.in);
+                    int c22;
+                    c22 = input22.nextInt();
+
+                    movieNode zNode = new movieNode(c20, c21, c22);
+                    bst.insert(zNode);
+                    bst.insertID(zNode);
+                    heap.insert(zNode);
+
+                    System.out.println("Here is your info!");
+                    System.out.println(zNode.getTitle());
+                    System.out.println(zNode.getRDate());
+                    System.out.println(zNode.getPopScore());
+
+                    //s for movieNode
+                    try {
+                        FileOutputStream fileOut = new FileOutputStream("moviesBST.txt");
+                        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+                        out.writeObject(bst);
+                        out.close();
+                        fileOut.close();
+                        System.out.println("Your file has been serialized!");
+                    } catch (IOException i) {
+                        i.printStackTrace();
+                    }
+
+                    try {
+                        FileOutputStream fileOut = new FileOutputStream("moviesHeap.txt");
+                        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+                        out.writeObject(heap);
+                        out.close();
+                        fileOut.close();
+                        System.out.println("Your file has been serialized!");
+                    } catch (IOException i) {
+                        i.printStackTrace();
+                    }
+                } else if (m1 == 2) {
+                    System.out.println("Enter ID: ");
+                    Scanner uInput = new Scanner(System.in);
+                    int i;
+
+                    i = uInput.nextInt();
+                    System.out.println("The movie with ID " + i + " is: ");
+
+                    //d for movieNode for search by ID
+                    try {
+                        FileInputStream fileIn = new FileInputStream("moviesBST.txt");
+                        ObjectInputStream in = new ObjectInputStream(fileIn);
+                        //heap = (netflixHeap) in.readObject();
+                        bst = (netflixBST) in.readObject();
+                        System.out.println(bst.searchID(i).getTitle());
+                        //System.out.println(heap.findMin().getTitle);
+
+                        in.close();
+                        fileIn.close();
+                        // return heap;
+                    } catch (IOException w) {
+                        w.printStackTrace();
+                        //  return null;
+                    } catch (ClassNotFoundException c) {
+                        System.out.println("Error: Class not found");
+                        c.printStackTrace();
+                        // return null;
+                    }
+                } else if (m1 == 3) {
+                    System.out.println("Access to WishList, Would you Like to:");
+                    System.out.println("(1) Add to WishList");
+                    System.out.println("(2) Access Next Movie");
+
+                    Scanner inM2 = new Scanner(System.in);
+                    int m2;
+                    m2 = inM2.nextInt();
+
+                    if (m2 == 1) {
+                        System.out.println("Adding a New Movie!");
+
+                        System.out.println("Please Input Title of movie:");
+                        Scanner input8 = new Scanner(System.in);
+                        String c8;
+                        c8 = input8.nextLine();
+
+                        System.out.println("Please Input Release Date of movie:");
+                        Scanner input9 = new Scanner(System.in);
+                        int c9;
+                        c9 = input9.nextInt();
+
+                        System.out.println("Please Input the Popularity Score:");
+                        Scanner input10 = new Scanner(System.in);
+                        int c10;
+                        c10 = input10.nextInt();
+
+                        movieNode mov4 = new movieNode(c8, c9, c10);
+
+                        bst.insert(mov4);
+                        bst.insertID(mov4);
+                        heap.insert(mov4);
+                    } else {
+                        //Acess to Next Movie
+                    }
+                } else if (m1 == 4) {
+                    //Print to Screen all Movies based on Release Date
+                    bst.traverse();
+                } else if (m1 == 5) {
+                    //Go Back
+                    System.out.println("Choose an action below: \n");
+                    System.out.println("(1) Access Movie (by Release Date)");
+                    System.out.println("(2) Access Movie(by ID)");
+                    System.out.println("(3) Access Wish List");
+                    System.out.println("(4) Print to Screen All Movies in Order of Release Date");
+                    System.out.println("(5) Go back");
+                    System.out.println("(6) Quit");
+
+                    Scanner input67 = new Scanner(System.in);
+                    int c67;
+                    c67 = input67.nextInt();
+                } else if (m1 == 7) {
+                    System.out.println("Enter Release Date:");
+                    Scanner uInput = new Scanner(System.in);
+                    int i;
+
+                    i = uInput.nextInt();
+
+                    //System.out.println(bst.search(i).getTitle());
+
+                    try {
+                        FileInputStream fileIn = new FileInputStream("moviesBST.txt");
+                        ObjectInputStream in = new ObjectInputStream(fileIn);
+                        //heap = (netflixHeap) in.readObject();
+                        bst = (netflixBST) in.readObject();
+                        //System.out.println(bst.searchID(i).getTitle());
+                        System.out.println(bst.search(i).getTitle());
+                        System.out.println("The movie with release date " + i + " is: ");
+                        in.close();
+                        fileIn.close();
+                        // return heap;
+                    } catch (IOException w) {
+                        w.printStackTrace();
+                        //  return null;
+                    } catch (ClassNotFoundException c) {
+                        System.out.println("Error: Class not found");
+                        c.printStackTrace();
+                        // return null;
+                    }
+                } else {
+                    System.out.println("Invaild Input. Please Try Again!");
+
+                    //yes i know its in the wrong place
+
+                }
+
             } else {
                 System.out.println("Error");
             }
@@ -363,8 +576,6 @@ public class netflix2 {
                 System.out.println("Invalid username and password");
                 netflix2.main(args);
             }
-
-
         } else {
             System.out.println("Invalid Input. Please Try Again!");
         }
